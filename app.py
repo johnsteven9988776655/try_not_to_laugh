4ffa8dfa03b07c0627670932fadb5265b0ad3cc5
johnsteven9988776655 <johnsteven9988776655>
2025-10-28 13:55:10 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, jsonify
+import time
+import random
+import string
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/phone_freezer')
+def phone_freezer():
+    """Ultimate phone freezing test - most aggressive"""
+    return render_template('phone_freezer.html')
+
+@app.route('/memory_bomb')
+def memory_bomb():
+    """Memory exhaustion test"""
+    return render_template('memory_bomb.html')
+
+@app.route('/cpu_intensive')
+def cpu_intensive():
+    """CPU intensive calculations"""
+    return render_template('cpu_intensive.html')
+
+@app.route('/api/heavy_data')
+def heavy_data():
+    """Generate massive JSON response"""
+    data = {
+        'payload': ''.join(random.choices(string.ascii_letters + string.digits, k=5000000)),
+        'timestamp': time.time()
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
