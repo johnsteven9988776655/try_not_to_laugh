70cd333940fdd29dfd2614ae82dc3945a575bed9
johnsteven9988776655 <johnsteven9988776655>
2025-10-28 14:31:42 +0300

    updated

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def heavy_data():
     return jsonify(data)
 
 # Only needed for local testing
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
